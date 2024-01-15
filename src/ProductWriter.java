005dfa1eb0a1b29aec03b64eb43c5552794afebd
aidenward6 <126358242+aidenward6@users.noreply.github.com>
2024-01-14 21:19:06 -0500

    ProductWriter.java

--- src/ProductWriter.java
+++ src/ProductWriter.java
@@ -11,11 +11,7 @@ public class ProductWriter {
 
     public static void main(String[] args) {
 
-        // a. ID (String)
-        // b. FirstName
-        // c. LastName
-        // d. title (Mr. Ms. etc)
-        // e. Year Of Birth (int)
+
         String ID = "";
         String name = "";
         String description = "";
@@ -28,7 +24,6 @@ public class ProductWriter {
 
         ArrayList<String> recs = new ArrayList<>();
         do {
-            //get five data fields
             ID = SafeInput.getNonZeroLenString(in, "Enter the ID");
             name = SafeInput.getNonZeroLenString(in, "Enter Name");
             description = SafeInput.getNonZeroLenString(in, "Enter Description");
@@ -48,24 +43,20 @@ public class ProductWriter {
         Path file = Paths.get(workingDirectory.getPath() + "ProductTestData.txt");
         try
         {
-            // Typical java pattern of inherited classes
-            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+         
             OutputStream out =
                     new BufferedOutputStream(Files.newOutputStream(file, CREATE));
             BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(out));
 
-            // Finally can write the file LOL!
 
             for(String rec : recs)
             {
-                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
-                writer.newLine();  // adds the new line
+                writer.write(rec, 0, rec.length());  
+                writer.newLine(); 
 
             }
-            writer.close(); // must close the file to seal it and flush buffer
+            writer.close(); 
             System.out.println("Data file written!");
         }
         catch (IOException e)
