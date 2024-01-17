5c84077643d6d86dfda3fc72618116bef712aef1
aidenward6 <126358242+aidenward6@users.noreply.github.com>
2024-01-16 23:20:52 -0500

    PersonReader.java

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,42 +1,73 @@
 import javax.swing.*;
-import java.nio.file.Files;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 
-public class PersonReader {
-    public static void main(String[] args) {
+public class PersonReader
+{
+    public static void main(String[] args)
+    {
         JFileChooser chooser = new JFileChooser();
         File selectedFile;
-        String rec = "";
+        String rec = "" ;
 
-        try {
+        ArrayList<String> lines = new ArrayList<>();
+
+        try
+        {
+
             File workingDirectory = new File(System.getProperty("user.dir"));
             chooser.setCurrentDirectory(workingDirectory);
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
                 selectedFile = chooser.getSelectedFile();
                 Path file = selectedFile.toPath();
+
                 InputStream in =
                         new BufferedInputStream(Files.newInputStream(file, CREATE));
                 BufferedReader reader =
                         new BufferedReader(new InputStreamReader(in));
 
+                // Finally we can read the file LOL!
                 int line = 0;
-                while (reader.ready()) {
+
+                String words[];
+
+                System.out.println("ID#       Firstname    Lastname       Title     YOB");
+                System.out.println("=====================================================");
+
+                while(reader.ready())
+                {
                     rec = reader.readLine();
+                    lines.add(rec);
                     line++;
-                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                    //System.out.println(String.format("%s%n", rec));
                 }
-                reader.close(); 
+
+
+
+                for(String l : lines) {
+                    String fields[] = l.split(", ");
+
+                    // for(String f:fields)
+                    System.out.printf("%-10s %-10s %-15s %-10s %-5s %n",fields[0],fields[1], fields[2], fields[3], fields[4]);
+
+                }
+
+                reader.close(); // must close the file to seal it and flush buffer
                 System.out.println("\n\nData file read!");
-
-
-            } else  
-            {
-                System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
             }
-        }
+            else  // user closed the file dialog without choosing
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }  // end of TRY
         catch (FileNotFoundException e)
         {
             System.out.println("File not found!!!");
@@ -48,8 +79,4 @@ public class PersonReader {
         }
     }
 
-
-
 }
-
-
